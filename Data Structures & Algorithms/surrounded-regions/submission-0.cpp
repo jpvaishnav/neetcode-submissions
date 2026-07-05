735246eeb5f911ce9fd2ae6e943dd77f81b9929f
Jay Prakash <46133803+jpvaishnav@users.noreply.github.com>
2026-07-05 18:06:44 +0530

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.cpp
@@ -0,0 +1,106 @@
+class Solution {
+public:
+
+    bool isValidNode(int i, int j, int m, int n)
+    {
+        return (i>=0 && j>=0 && i<m && j<n);
+    }
+
+    int row_directions[4] = {0,0,-1,1};
+    int col_directions[4] = {-1,1,0,0};
+    
+    void DFS(vector<vector<char>>& board,
+            int i, int j, int m, int n)
+    {
+        cout << " entering dfs "<< i << " " << j << endl;
+        // mark the node as visited, by marking o to Y (so that in end, we can switch back to o)
+        board[i][j] = 'Y';
+
+        for (int k = 0; k < 4; k++)
+        {
+            int row_index = i + row_directions[k];
+            int col_index = j + col_directions[k];
+            if(isValidNode(row_index, col_index, m, n) && board[row_index][col_index] == 'O')
+            {
+                DFS(board, row_index, col_index, m, n);
+            }
+        }
+    }
+    void solve(vector<vector<char>>& board) {
+        /*
+        Approach: 
+            if any of edge node is open, and if it is connected to any other open nodes, 
+                then that node is also not surrounded
+            So, un-surroundable nodes can be found out by applying DFS on edge open nodes
+            and temporarily updating them to N
+            After one traversal, the nodes which are still O in grid are surround and can be updated to X
+            and re-update N to X before returning.
+
+            Pattern: Based on question example, visualisations
+            Time complexity: O(M*N)
+        */
+
+        int m = board.size();
+        if(m==0)
+        {
+            return;
+        }
+        int n = board[0].size();
+
+        // traverse all corner elements
+        for(int j = 0; j < n; j++)
+        {
+            // 1st and last row
+            if(board[0][j] == 'O')
+            {
+                cout << "starting dfs: "<< 0 << " "<< j << endl;
+                DFS(board, 0, j, m, n);
+            }
+            if(board[m-1][j] == 'O')
+            {
+                cout << "starting dfs: "<< m-1 << " "<< j << endl;
+                DFS(board, m-1, j, m, n);
+            }
+        }
+
+        // 1st and last column
+        for(int i = 0; i < m ; i++)
+        {
+            // j = 0 and j = n-1
+            if(board[i][0] == 'O')
+            {
+                cout << "starting dfs: "<< i << " "<< 0 << endl;
+                DFS(board, i, 0, m, n);
+            }
+            if(board[i][n-1] == 'O')
+            {
+                cout << "starting dfs: "<< i << " "<< n-1 << endl;
+                DFS(board, i, n-1, m, n);
+            } 
+        }
+
+        // now all o can be surrounded by x
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(board[i][j] == 'O')
+                {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+
+        // restore all y un-surroundabled by o
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(board[i][j] == 'Y')
+                {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+};
