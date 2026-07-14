b23ee46cc535827d915f78ca14e56fb17f015027
Jay Prakash <46133803+jpvaishnav@users.noreply.github.com>
2026-07-14 19:32:02 +0530

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        /*
+        Approach: Similar to train platform problem
+        sort both start and end arrays
+        Pick the smaller of start or end
+            - increment total counter if picking from start array
+            - decrement total counter if picking from finish array
+            do ans = max(ans, total counter value)
+
+        Time complexity: O(nLogn)
+
+        Question: What is difference between approaches for
+            > Non-overlapping intervals: sort by finish time
+            > Meeting rooms: Sort both start and finish time
+            > Merge overlapping intervals: sort by start time
+
+        */
+
+        vector<int> start_times;
+        vector<int> finish_times;
+
+        for(auto interval: intervals)
+        {
+            start_times.push_back(interval.start);
+            finish_times.push_back(interval.end);
+        }
+
+        sort(start_times.begin(), start_times.end());
+        sort(finish_times.begin(), finish_times.end());
+
+        int i=0;
+        int j=0;
+
+        int n = start_times.size();
+
+        int counter = 0;
+        int ans = 0;
+
+        while(i<n && j<n)
+        {
+            if(start_times[i]<finish_times[j])
+            {
+                i++;
+                counter++;
+            }
+            else
+            {
+                j++;
+                counter--;
+            }
+            ans = max(ans, counter);
+        }
+        return ans;
+    }
+};
